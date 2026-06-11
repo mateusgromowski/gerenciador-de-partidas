89f17a68239008851fb85e98fe2f2bdfaa98b0fc
Mateus Gromowski <mateusbarbosagromowski@gmail.com>
2026-06-11 11:08:21 -0300

    chore: configuração inicial do projeto

---
+++ matchmanager/src/main/java/com/mateus/matchmanager/MatchmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.mateus.matchmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatchmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatchmanagerApplication.class, args);
+	}
+
+}
---
+++ matchmanager/src/test/java/com/mateus/matchmanager/MatchmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mateus.matchmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatchmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
